7e0f3536be23de130b4269cb60b28df48a68ac48
yusiny <yusinkim.or@gmail.com>
2022-10-12 02:00:49 +0900

    test: 카메라 추가

--- src/Game.js
+++ src/Game.js
@@ -3,15 +3,16 @@ import { Canvas } from "@react-three/fiber";
 
 import "./styles/game.css";
 import Diglett from "./components/Diglett";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import Grass from "./components/Grass";
 
 export default function Game() {
   return (
     <div id="game">
-      <div> 게임 화면 </div>
+      <div> 게임 화면 2</div>
       <Canvas>
         <OrbitControls />
+        <PerspectiveCamera makeDefault fov={90} position={[0, 4, 10]} />
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 15, 10]} angle={0.3} />
 
