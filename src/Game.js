c9cf91dc86fa8ee729272d787776e69117668bab
김종헌 <klego98@gmail.com>
2022-10-13 20:34:15 +0900

    add hammer.glb

--- src/Game.js
+++ src/Game.js
@@ -3,6 +3,7 @@ import { Canvas } from "@react-three/fiber";
 
 import "./styles/game.css";
 import Diglett from "./components/Diglett";
+import Diglett2 from "./components/Diglett";
 import { OrbitControls } from "@react-three/drei";
 import Grass from "./components/Grass";
 
@@ -15,8 +16,9 @@ export default function Game() {
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 15, 10]} angle={0.3} />
 
-        <Diglett position={[0, -1, 0]} scale={[5, 5, 5]} />
-        <Grass position={[0, -1.5, 0]} />
+        <Diglett position={[-3, -1, 0]} scale={[5, 5, 5]} />
+        <Diglett2 position={[3, -1, 0]} scale={[5, 5, 5]} />
+        <Grass position={[0, -1, 0]} />
       </Canvas>
     </div>
   );
