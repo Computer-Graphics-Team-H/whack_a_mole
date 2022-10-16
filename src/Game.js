0c2f36184849c9acd63a9458a9316924720023ae
김종헌 <klego98@gmail.com>
2022-10-16 17:43:47 +0900

    copy glb files

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import "./styles/game.css";
 import Diglett from "./components/Diglett";
 import Hammer from "./components/Toy_hammer";
-import Diglett2 from "./components/Diglett";
+import Diglett2 from "./components/Diglett copy";
+import Diglett3 from "./components/Diglett copy 2";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import Grass from "./components/Grass";
 
 export default function Game() {
+  
   return (
     <div id="game">
       <div> 게임 화면 </div>
@@ -16,11 +18,20 @@ export default function Game() {
         <PerspectiveCamera makeDefault fov={90} position={[0, 4, 10]} />
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 15, 10]} angle={0.3} />
+        <Suspense fallback={null}>
+          {/* <Hammer position={[0,0,0]} scale={[1,1,1]}/>         */}
+          <Diglett position={[0, 0, 0]} scale={[5, 5, 5]} />
+          <Diglett2 position={[6, 0, 0]} scale={[5, 5, 5]} />
+          <Diglett3 position={[-6, 0, 0]} scale={[5, 5, 5]} />
+          {/* <Diglett4 position={[0, 0, 0]} scale={[5, 5, 5]} />
+          <Diglett5 position={[6, 0, 0]} scale={[5, 5, 5]} />
+          <Diglett6 position={[-6, 0, 0]} scale={[5, 5, 5]} />
+          <Diglett7 position={[0, 0, 0]} scale={[5, 5, 5]} />
+          <Diglett8 position={[6, 0, 0]} scale={[5, 5, 5]} />
+          <Diglett9 position={[-6, 0, 0]} scale={[5, 5, 5]} /> */}
+          <Grass position={[0, -1, 0]} scale ={[5,5,5]}/>
+        </Suspense>
 
-        <Hammer position={[0,0,0]} scale={[1,1,1]}/>        
-        <Diglett position={[-3, -1, 0]} scale={[5, 5, 5]} />
-        <Diglett2 position={[3, -1, 0]} scale={[5, 5, 5]} />
-        <Grass position={[0, -1, 0]} />
       </Canvas>
     </div>
   );
