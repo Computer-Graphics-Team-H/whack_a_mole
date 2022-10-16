f1391515786953094daf561e4bdaea754078674e
KimJongHeon <klego98@gmail.com>
2022-10-16 19:20:52 +0900

    moles add

--- src/Game.js
+++ src/Game.js
@@ -3,8 +3,15 @@ import { Canvas } from "@react-three/fiber";
 import "./styles/game.css";
 import Diglett from "./components/Diglett";
 import Hammer from "./components/Toy_hammer";
+import Hammer2 from "./components/Cartoon_hammer";
 import Diglett2 from "./components/Diglett copy";
 import Diglett3 from "./components/Diglett copy 2";
+import Diglett4 from "./components/Diglett copy 3";
+import Diglett5 from "./components/Diglett copy 4";
+import Diglett6 from "./components/Diglett copy 5";
+import Diglett7 from "./components/Diglett copy 6";
+import Diglett8 from "./components/Diglett copy 7";
+import Diglett9 from "./components/Diglett copy 8";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import Grass from "./components/Grass";
 
@@ -23,12 +30,12 @@ export default function Game() {
           <Diglett position={[0, 0, 0]} scale={[5, 5, 5]} />
           <Diglett2 position={[6, 0, 0]} scale={[5, 5, 5]} />
           <Diglett3 position={[-6, 0, 0]} scale={[5, 5, 5]} />
-          {/* <Diglett4 position={[0, 0, 0]} scale={[5, 5, 5]} />
-          <Diglett5 position={[6, 0, 0]} scale={[5, 5, 5]} />
-          <Diglett6 position={[-6, 0, 0]} scale={[5, 5, 5]} />
-          <Diglett7 position={[0, 0, 0]} scale={[5, 5, 5]} />
-          <Diglett8 position={[6, 0, 0]} scale={[5, 5, 5]} />
-          <Diglett9 position={[-6, 0, 0]} scale={[5, 5, 5]} /> */}
+          <Diglett4 position={[0, 0, 6]} scale={[5, 5, 5]} />
+          <Diglett5 position={[-6, 0, 6]} scale={[5, 5, 5]} />
+          <Diglett6 position={[6, 0, 6]} scale={[5, 5, 5]} />
+          <Diglett7 position={[6, 0, -6]} scale={[5, 5, 5]} />
+          <Diglett8 position={[0, 0, -6]} scale={[5, 5, 5]} />
+          <Diglett9 position={[-6, 0, -6]} scale={[5, 5, 5]} />
           <Grass position={[0, -1, 0]} scale ={[5,5,5]}/>
         </Suspense>
 
